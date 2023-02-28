d0ee4c52a316d1adba93f2b0f465cd7a7ad1b938
Anna <anna4776497@gmail.com>
2023-02-28 14:39:32 +0300

    2.1

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,13 @@ function App() {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
   const [selectTrain, setSelectTrain] = useState<string>("");
-  const [speedLimitList, setSpeedLimitList] = useState<Array<ISpeedLimit> | []>([])
+  const [speedLimitList, setSpeedLimitList] = useState<Array<ISpeedLimit> | []>(
+    []
+  );
   const [change, setChange] = useState<boolean>(false);
+  const [upd, setUpd] = useState<boolean>(false);
 
-  useEffect(() => {
+  const pushTrains = () => {
     getTrains()
       .then((res) => {
         setTrains(res);
@@ -34,61 +37,77 @@ function App() {
       .catch((err) => {
         setError(true);
       });
+  };
+
+  useEffect(() => {
+    pushTrains();
+    setUpd(false);
+  }, [upd]);
+
+  useEffect(() => {
+    pushTrains();
   }, []);
 
   const handleClick = (name: string) => {
     if (selectTrain === name) {
       setSelectTrain("");
       setSpeedLimitList([]);
-      setChange(false)
+      setChange(false);
     } else {
       setSelectTrain(name);
-      const arr: any = (trains.filter(i => i.name === name))[0].speedLimits
+      const arr: any = trains.filter((i) => i.name === name)[0].speedLimits;
       setSpeedLimitList(arr);
-      setChange(false)
+      setChange(false);
     }
   };
 
-  const onHandleInput = (e:any, i: string) => {
+  const onHandleInput = (e: any, i: string) => {
     if (!Number.isInteger(+e.target.value)) {
-      alert('Только целые числа')
-      return
+      alert("Только целые числа");
+      return;
     }
 
-    if (+e.target.value===0) {
-      alert('Только целые числа больше 0')
-      return
+    if (+e.target.value === 0) {
+      alert("Только целые числа больше 0");
+      return;
     }
 
-    setSpeedLimitList(speedLimitList.map((item) => {
-      if (item.name === i) {
-        return {
-          ...item,
-          speedLimit: +e.target.value
+    setSpeedLimitList(
+      speedLimitList.map((item) => {
+        if (item.name === i) {
+          return {
+            ...item,
+            speedLimit: +e.target.value,
+          };
         }
-      }
-      return item
-    }))
-    setChange(true)
-  }
+        return item;
+      })
+    );
+    setChange(true);
+  };
 
   const onSendChanges = () => {
-    const sortArr: ISpeedLimit[] = speedLimitList.sort((a, b) => (a.speedLimit < b.speedLimit ? -1 : 1))
-    const desc: string = trains.filter(item => item.name === selectTrain)[0].description
+    const sortArr: ISpeedLimit[] = speedLimitList.sort((a, b) =>
+      a.speedLimit < b.speedLimit ? -1 : 1
+    );
+    const desc: string = trains.filter((item) => item.name === selectTrain)[0]
+      .description;
 
-    let data:ITrains = {
+    let data: ITrains = {
       name: selectTrain,
       description: desc,
-      speedLimits: sortArr
-    }
-    console.log('Отправка данных на сервер:')
-    console.log(data)
-      putSpeeds(data)
-  }
+      speedLimits: sortArr,
+    };
+    console.log("Отправка данных на сервер:");
+    console.log(data);
+    putSpeeds(data);
+    setChange(false);
+    setUpd(true);
+  };
 
   const drowBtn = () => {
-   return  change ? "btn-send": "btn" 
-  }
+    return change ? "btn-send" : "btn";
+  };
 
   return (
     <div className="container">
@@ -102,13 +121,22 @@ function App() {
           handleClick={handleClick}
           selectTrain={selectTrain}
         />
-        {selectTrain.length > 0 ? <TrainItem speedLimits={speedLimitList} onHandleInput={onHandleInput} /> : <div className="select-train">Выберите поезд</div>}  
-        <button className={drowBtn()}
+        {selectTrain.length > 0 ? (
+          <TrainItem
+            speedLimits={speedLimitList}
+            onHandleInput={onHandleInput}
+          />
+        ) : (
+          <div className="select-train">Выберите поезд</div>
+        )}
+        <button
+          className={drowBtn()}
           disabled={!change}
           onClick={() => onSendChanges()}
-        >Отправить изменения</button>
+        >
+          Отправить изменения
+        </button>
       </div>
-       
     </div>
   );
 }
